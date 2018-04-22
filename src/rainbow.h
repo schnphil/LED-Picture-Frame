502c5a318c60e00e1dd48dc523f494548ca8d5c3
mnebuhr <nebuhr@uni-bremen.de>
2018-04-22 12:39:02 +0200

    Docs
    An der Dokumenttion gearbeitet

--- src/rainbow.h
+++ src/rainbow.h
@@ -4,6 +4,17 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+/**
+* Fuellt die LEDs in Regenbogenfarbe entlang des HUE Wertes.
+*
+* @Author: mnebuhr
+* @Since : 20.04.2018
+
+* Parameter:
+* leds     : Zeiger auf den Speicherbereich fuer die Farbwerte der LEDs.
+* num_leds : Anzahl der LEDs
+* hue      : Startwert des Regenbogens.
+*/
 void rainbow(CRGB* leds, uint8_t num_leds, uint8_t hue);
 void addGlitter(CRGB* leds, uint8_t num_leds, fract8 chanceOfGlitter);
 void rainbowWithGlitter(CRGB* leds, uint8_t num_leds, uint8_t hue, fract8 chanceOfGlitter);
