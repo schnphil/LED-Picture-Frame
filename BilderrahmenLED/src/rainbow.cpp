7ca2fba090dbdf0b59013084d985fe999d6df5a8
mnebuhr <nebuhr@uni-bremen.de>
2018-04-24 11:13:36 +0200

    add some new mBolcks ans demo.sb2

--- BilderrahmenLED/src/rainbow.cpp
+++ BilderrahmenLED/src/rainbow.cpp
@@ -25,3 +25,31 @@ void confetti(CRGB* leds, uint8_t num_leds, uint8_t hue)
   int pos = random16(num_leds);
   leds[pos] += CHSV( hue + random8(64), 200, 255);
 }
+void sinelon(CRGB* leds, uint8_t num_leds, uint8_t hue)
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, num_leds, 20);
+  int pos = beatsin16( 13, 0, num_leds-1 );
+  leds[pos] += CHSV( hue, 255, 192);
+}
+
+void bpm(CRGB* leds, uint8_t num_leds, uint8_t hue)
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < num_leds; i++) { //9948
+    leds[i] = ColorFromPalette(palette, hue+(i*2), beat-hue+(i*10));
+  }
+}
+
+void jungle(CRGB* leds, uint8_t num_leds) {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, num_leds, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, num_leds-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
--- BilderrahmenLED/src/rainbow.h
+++ BilderrahmenLED/src/rainbow.h
@@ -8,5 +8,8 @@ void rainbow(CRGB* leds, uint8_t num_leds, uint8_t hue);
 void addGlitter(CRGB* leds, uint8_t num_leds, fract8 chanceOfGlitter);
 void rainbowWithGlitter(CRGB* leds, uint8_t num_leds, uint8_t hue, fract8 chanceOfGlitter);
 void confetti(CRGB* leds, uint8_t num_leds, uint8_t hue); 
+void sinelon(CRGB* leds, uint8_t num_leds, uint8_t hue); 
+void bpm(CRGB* leds, uint8_t num_leds, uint8_t hue); 
+void jungle(CRGB* leds, uint8_t num_leds); 
 
 #endif
